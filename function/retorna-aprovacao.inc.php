05dab2dcbb678f999ece1441dbfb6f7213fec598
Luciano Barros <lucianof.barros89@gmail.com>
2024-07-03 16:24:21 -0300

    Enviando refatoracao do if/else e index.php

--- function/retorna-aprovacao.inc.php
+++ function/retorna-aprovacao.inc.php
@@ -7,11 +7,9 @@ function retorna_aprovacao($media, $nota3, $nota4) {
     if ($media >= 7) {
         return true;
     }
-
     if ($media >= 6 && $nota4 >= 8) {
         return true;
     }
-
     if ($media >= 5 && $soma_nota_tres_e_nota_quatro / $somaPesos >= 8) {
         return true;
     }
