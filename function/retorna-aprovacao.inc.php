6033f19676630609b1d4b5d6f7e6c96da64e8fd8
Luciano Barros <lucianof.barros89@gmail.com>
2024-07-03 12:59:45 -0300

    corrigindo boolean

--- function/retorna-aprovacao.inc.php
+++ function/retorna-aprovacao.inc.php
@@ -7,11 +7,11 @@ function retorna_aprovacao($media, $nota3, $nota4) {
     $aprovacao = false;
 
     if ($media >= 7) {
-        $aprovacao = 'true';
+        $aprovacao = true;
     } elseif ($media >= 6 && $nota4 >= 8) {
-        $aprovacao = 'true';
+        $aprovacao = true;
     } elseif ($media >= 5 && $soma_nota_tres_e_nota_quatro / $somaPesos >= 8) {
-        $aprovacao = 'true';
+        $aprovacao = true;
     }
 
     return $aprovacao;
