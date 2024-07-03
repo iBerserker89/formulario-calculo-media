2e86f23835920a148fb68ed1c6b14f5b6eb02611
Luciano Barros <lucianof.barros89@gmail.com>
2024-07-03 13:23:39 -0300

    refatorando retorna-aprovacao

--- function/retorna-aprovacao.inc.php
+++ function/retorna-aprovacao.inc.php
@@ -4,17 +4,17 @@ function retorna_aprovacao($media, $nota3, $nota4) {
     $somaPesos = 7;
     $soma_nota_tres_e_nota_quatro = ($nota3 * 3) + ($nota4 * 4);
 
-    $aprovacao = false;
-
     if ($media >= 7) {
-        $aprovacao = true;
-    } elseif ($media >= 6 && $nota4 >= 8) {
-        $aprovacao = true;
-    } elseif ($media >= 5 && $soma_nota_tres_e_nota_quatro / $somaPesos >= 8) {
-        $aprovacao = true;
+        return true;
     }
 
-    return $aprovacao;
+    if ($media >= 6 && $nota4 >= 8) {
+        return true;
+    }
+
+    if ($media >= 5 && $soma_nota_tres_e_nota_quatro / $somaPesos >= 8) {
+        return true;
+    }
+
+    return false;
 }
-
-
